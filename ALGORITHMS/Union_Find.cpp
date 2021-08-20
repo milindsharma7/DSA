e599a38663c85d754ae635eab1849c5d0ce2ef06
Milind <milindsharmagithub@gmail.com>
2021-08-20 15:30:42 +0530

    completed union find algorithm

--- ALGORITHMS/Union_Find.cpp
+++ ALGORITHMS/Union_Find.cpp
@@ -16,21 +16,31 @@ int Find(vector<pair<int,int>> &dsu,int u){
     return Find(dsu,dsu[u].first);
 }
 
-int Union(vector<pair<int,int>> &dsu,int u,int v){
+bool Union(vector<pair<int,int>> &dsu,int u,int v){
 
     int f1=Find(dsu,u);
     int f2=Find(dsu,v);
+
+    if(f1==f2)
+        return true;
     
-    if(dsu[f2].second>=dsu[f1].second)
+    if(dsu[u].second>=dsu[v].second)
     {
-        dsu[f1].first=f2;
+        dsu[v].first=u;
+        Make_Set(dsu,v);
+    }
+    else
+    {
+        dsu[u].first=v;
         Make_Set(dsu,u);
     }
+
+    return false;
 }
 
 int main(){
-    cout<<"Enter number of edges and vertices in graph"<<endl;
-    int n; cin>>n;
+    cout<<"Enter number vertices in graph"<<endl;
+    int n;cin>>n;
     cout<<"Enter number of edges"<<endl;
     int m;cin>>m;
     cout<<"Enter all the edges"<<endl;
@@ -43,9 +53,16 @@ int main(){
         dsu[i].second=0;
     }
 
+    bool contains_cycle=false;
     for(int i=0;i<m;i++)
     {
-
+        int u,v;cin>>u>>v;
+        if(Union(dsu,u,v))
+            contains_cycle=true;
     }
+
+    if(contains_cycle)cout<<"Graph contains cycle"<<endl;
+    else cout<<"Graph does not contain cycle"<<endl;
+    
     return 0;
 }
