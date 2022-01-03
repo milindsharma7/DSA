3ba96965f2f8627bf474b494fb32ea873ac94083
Milind <milindsharmagithub@gmail.com>
2022-01-04 01:32:08 +0530

    Fenwick Tree Update

---
+++ DATA STRUCTURES/Fenwick_Tree_2.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long int;
+
+class FenwickTree{
+    private:
+        vector <ll> Tree;
+        vector <ll> Prefix;
+    public:
+        FenwickTree(vector <ll> &arr){
+            ll n = arr.size();
+            Tree.resize(n+1,0);
+            Prefix.resize(n+1,0);
+            for(ll i=1;i<=n;i++){
+                Prefix[i] = Prefix[i-1] + arr[i-1]; 
+            }
+            for(ll i=1;i<=n;i++){
+               Tree[i] = Prefix[i] - Prefix[i-(i&-i)];  
+            }
+        }
+        void build(ll idx,ll num){
+            ll sum = 0;
+            while(idx<Tree.size()){
+                Tree[idx] += num;
+                idx += (idx&-idx);
+            }
+        }
+        long long int prefix(ll idx){
+            ll sum = 0;
+            while(idx>0){
+                sum += Tree[idx];
+                idx -= (idx&-idx);
+            }
+            return sum;
+        }
+        long long int query(ll l,ll r){
+            return prefix(r) - prefix(l-1);
+        }
+        void update(ll idx,ll num){
+            ll old = query(idx,idx);
+            ll delta = num - old;
+            build(idx,delta);
+        }
+};
+
+int main(){
+    /*      FENWICK TREE/BINARY INDEXED TREE
+            1)  QUERY(l,r) -> return sum of the range (l,r) (one based indexing)
+            2)  UPDATE(idx,num) -> updates the number at idx with num (one based indexing)
+            3)  Pre-Processing in O(n) using Prefix sum array
+    */
+    vector <ll> v = {1,2,3,4,5};
+    FenwickTree F(v);
+    cout<<F.query(1,2)<<endl;
+    cout<<F.query(3,3)<<endl;
+    cout<<F.query(2,5)<<endl;
+    F.update(4,69);
+    cout<<F.query(3,5)<<endl;
+    F.update(2,420);
+    cout<<F.query(1,5)<<endl;
+    return 0;
+}
